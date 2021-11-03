c67b0545c7ea76c8dcc027b4b138c8a00928c99e
Benjamin Weinstock <info@flimmerkiste.it>
2021-11-04 00:08:32 +0100

    :art: Change hiring section

--- src/screens/Hiring.js
+++ src/screens/Hiring.js
@@ -4,22 +4,42 @@ import Container from "../components/Container";
 import CV from "../assets/BWeinstockCV2020.pdf";
 import HeaderImg from "../components/HeaderImg";
 import { Superhero } from "../assets/emojis";
+import AmbientLogo from "../assets/logos/ambient-logo.svg";
+
 const Hiring = () => {
   return (
     <Container id="cv">
       <h2>
-        <HeaderImg src={Superhero} alt="you are" />
+        <HeaderImg src={Superhero} alt="Hiring Superhero" />
          hiring?
       </h2>
+      <p>
+        ... for now iam happy at
+        <Ambient href="https://ambient.digital">
+          <img src={AmbientLogo} alt="Ambient Digital Logo" />
+          mbient Digital
+        </Ambient>
+      </p>
+      <span>Still interested? ;)</span>
       <Download href={CV} target="_blank">
         Get my CV!
       </Download>
+      <span>Version: Jan 2020</span>
       <p>
         and write me an <a href="#contact">email</a>
       </p>
     </Container>
   );
 };
+
+const Ambient = styled.a`
+  img {
+    height: 2rem;
+    transform: translateY(2px);
+    margin: auto 0.1rem auto 0.5rem;
+  }
+  color: var(--text-secondary);
+`;
 
 const Download = styled.a`
   font-size: 1.1rem;
