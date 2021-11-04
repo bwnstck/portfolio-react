5ddf335134e25b9ad154ec2021fefbf256e046bf
Benjamin Weinstock <info@flimmerkiste.it>
2021-11-04 02:47:28 +0100

    Revert ":art: Add codesplitting"
    This reverts commit e032e6e9bc781008c205ad64e4986601906b6150.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { lazy, useState } from "react";
+import { useState } from "react";
 
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
@@ -6,13 +6,13 @@ import Wrapper from "./components/Wrapper";
 import GlobalStyle from "./GlobalStyles";
 
 import Welcome from "./screens/Welcome";
+import Hiring from "./screens/Hiring";
+import Tech from "./screens/Tech";
+import Projects from "./screens/Projects";
+import AboutMe from "./screens/AboutMe";
+import Contact from "./screens/Contact";
 
 import { isInViewPort } from "./screens/lib/responsiveHelpers";
-const Hiring = lazy(() => import("./screens/Hiring"));
-const Tech = lazy(() => import("./screens/Tech"));
-const Projects = lazy(() => import("./screens/Projects"));
-const AboutMe = lazy(() => import("./screens/AboutMe"));
-const Contact = lazy(() => import("./screens/Contact"));
 
 function App() {
   let welcome, cv, tech, projects, about, contact;
